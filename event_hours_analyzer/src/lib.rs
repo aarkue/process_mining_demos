6c0e2285f9c8da9871412a1542336acc152fad94
aarkue <mail@aarkue.eu>
2024-06-29 14:16:57 +0200

    Update event_hours_analyzer

--- event_hours_analyzer/src/lib.rs
+++ event_hours_analyzer/src/lib.rs
@@ -1,39 +1,35 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use process_mining::event_log::import_xes::build_ignore_attributes;
 use process_mining::event_log::XESEditableAttribute;
-use process_mining::{stream_xes_from_path, XESImportOptions};
+use process_mining::{import_xes_file, XESImportOptions};
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn get_num_events_per_hour(
-    xes_path: String,
-) -> PyResult<HashMap<String, usize>> {
+fn get_num_events_per_hour(xes_path: String) -> PyResult<HashMap<String, usize>> {
+    let log = import_xes_file(
+        &xes_path,
+        XESImportOptions {
+            ignore_log_attributes_except: Some(HashSet::default()),
+            ignore_trace_attributes_except: Some(HashSet::default()),
+            ignore_event_attributes_except: Some(build_ignore_attributes(vec!["time:timestamp"])),
+            ..XESImportOptions::default()
+        },
+    )
+    .unwrap();
     let mut map = HashMap::new();
-
-    let (mut log_stream, log_data) =
-        stream_xes_from_path(&xes_path, XESImportOptions::default()).unwrap();
-    let classifier = log_data
-        .classifiers
-        .iter()
-        .find(|c| c.name == "Event Name")
-        .unwrap();
-    let mut event_attrs_to_import = classifier.keys.clone();
-    event_attrs_to_import.extend(vec!["time:timestamp".to_string()]);
-    log_stream.set_options(XESImportOptions {
-        ignore_event_attributes_except: Some(build_ignore_attributes(&event_attrs_to_import)),
-        ignore_trace_attributes_except: Some(build_ignore_attributes(Vec::<&str>::new())),
-        sort_events_with_timestamp_key: Some("time:timestamp".to_string()),
-        ..XESImportOptions::default()
-    });
-    log_stream.for_each(|t| {
+    for t in &log.traces {
         for e in &t.events {
-            if let Some(date) = e.attributes.get_by_key("time:timestamp").and_then(|a| a.value.try_as_date()) {
+            if let Some(date) = e
+                .attributes
+                .get_by_key("time:timestamp")
+                .and_then(|a| a.value.try_as_date())
+            {
                 let ds = date.time().format("%H").to_string();
                 *map.entry(ds).or_default() += 1;
             }
         }
-    });
+    }
     Ok(map)
 }
 

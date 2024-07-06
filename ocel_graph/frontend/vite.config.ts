b50587fc73964bc3e2a66bda31a8ac043f6c563a
aarkue <mail@aarkue.eu>
2024-07-06 20:02:16 +0200

    Include OCEL Graph

--- ocel_graph/frontend/vite.config.ts
+++ ocel_graph/frontend/vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { outDir: "../docs/ocel_graph/", emptyOutDir: true,},
+  base: '',
   plugins: [react()],
   resolve: {
     alias: {

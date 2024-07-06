491cd27306d9b6a102c4ce2eaac9178050aa8f3c
aarkue <mail@aarkue.eu>
2024-07-06 20:04:29 +0200

    Fix: Correctly include ocel_graph

--- ocel_graph/frontend/vite.config.ts
+++ ocel_graph/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { outDir: "../docs/ocel_graph/", emptyOutDir: true,},
+  build: { outDir: "../../docs/ocel_graph/", emptyOutDir: true,},
   base: '',
   plugins: [react()],
   resolve: {
